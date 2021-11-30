321e85b7f6121f8df71e547e362b609440bb9a88
Alexander Zinov <sashkent3@gmail.com>
2021-12-01 01:52:50 +0300

    visual enhancements and refactoring

--- corruption/single_tile_corruption.go
+++ corruption/single_tile_corruption.go
@@ -17,7 +17,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/assets/sprites.atlas\"\n"
-  "default_animation: \"test_hex_bug_anim\"\n"
+  "default_animation: \"empty_hex\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
