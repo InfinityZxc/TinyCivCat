60927961b8cf8db361e995456beee20a7851b149
spoker28 <skarbaev2@gmail.com>
2022-01-16 22:55:21 +0300

    True beta of group vizualization

--- group_of_tile/group_of_tile.go
+++ group_of_tile/group_of_tile.go
@@ -17,7 +17,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/assets/sprites.atlas\"\n"
-  "default_animation: \"group\"\n"
+  "default_animation: \"group_empty\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
